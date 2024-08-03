bfa689cfe3c90812b98489c93edf09594bf80214
NataliaFerreiraVentura <nataliaferreiraventura@gmail.com>
2024-08-03 17:23:15 -0300

    Adicionando cenario de teste com credenciais in

--- cypress/e2e/features/login.feature
+++ cypress/e2e/features/login.feature
@@ -5,3 +5,9 @@ Feature: Login
     When o usuário insere o nome de usuário e a senha
     And o usuário clica no botão "LOGIN"
     Then o usuário deve ser redirecionado para a página inicial
+
+  Scenario: Usuário tenta fazer login com credenciais inválidas
+    Given o usuário está na página de login
+    When o usuário insere credenciais inválidas
+    And o usuário clica no botão "LOGIN"
+    Then o usuário deve ver uma mensagem de erro "Epic sadface: Username and password do not match any user in this service"
--- cypress/e2e/step_definitions/login.js
+++ cypress/e2e/step_definitions/login.js
@@ -1,11 +1,21 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+// #region Passos compartilhados
+
 // Acessa a pagina antes de qualquer interação
 Given('o usuário está na página de login', () => {
   cy.visit('https://www.saucedemo.com/v1/') 
 })
 
-// Realiza o login na aplicação
+// Clica no botão de login
+When('o usuário clica no botão {string}', (buttonText) => {
+  cy.get('#login-button').contains(buttonText).click()
+})
+// #endregion
+
+// #region Login com sucesso
+
+// Realiza o login na aplicação com credenciais validas
 When('o usuário insere o nome de usuário e a senha', () => {
   const username = 'standard_user'
   const password = 'secret_sauce'
@@ -14,11 +24,24 @@ When('o usuário insere o nome de usuário e a senha', () => {
   cy.get('#password').type(password)
 })
 
-When('o usuário clica no botão {string}', (buttonText) => {
-   cy.get('#login-button').contains(buttonText).click()
-})
-
-//Valida se a ação foi concluida
+//Verifica se o usuário foi redirecionado para a página inicial
 Then('o usuário deve ser redirecionado para a página inicial', () => {
   cy.url().should('include', '/v1/')
+}) 
+// #endregion
+ 
+// #region Login com dados inválidos
+
+When('o usuário insere credenciais inválidas', () => {
+  const username = 'invalid_user'
+  const password = 'invalid_password'
+
+  cy.get('#user-name').type(username)
+  cy.get('#password').type(password)
 })
+
+// Valida se o usuário vê uma mensagem de erro
+Then('o usuário deve ver uma mensagem de erro {string}', (errorMessage) => {
+  cy.get('[data-test="error"]').should('contain.text', errorMessage)
+})
+// #endregion
